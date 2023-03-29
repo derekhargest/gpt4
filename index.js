fa36123ce94d372f60d3c3035ea04fa386af47f4
derekhargest <derekhargest@gmail.com>
2023-03-29 13:30:16 -0400

    change prompt

--- index.js
+++ index.js
@@ -27,25 +27,29 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the public folder
 
 app.post("/", async (req, res) => {
-	const { messages } = req.body;
-  
-	console.log(messages);
-  
-	const completion = await openai.createChatCompletion({
-	  model: "gpt-3.5-turbo",
-	  messages: [
-		{
-		  role: "system",
-		  content:
-			"Youre a wise yogi in india. you have studied with all of the great minds on meditation and have read all of the books on meditation you can. I am going to ask you pointed questions about the nature of the warld and conciousness and i want you to respond to me like a wise yogi in the buddhist religion.",
-		},
-		...messages,
-	  ],
-	});
-	res.json({
-	  completion: completion.data.choices[0].message,
-	});
-  });
+    const { messages } = req.body;
+
+    console.log(messages);
+
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "system",
+                content:
+                    "You are a chatbot that is an expert on my business, I am a freelande web developer. Ive been creating websites for over 10 years. I work primarily in wordpress and shopify. I have expierence with html, scc, javascript, node.js, react and more. I would like ytou to respond with a short intro paragraph for my website. No longer than a sentence.",
+            },
+            ...messages,
+        ],
+    });
+
+    const responseData = {
+        completion: completion.data.choices[0].message,
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(responseData));
+});
   
   app.get("/", (req, res) => {
 	// Serve the index.html file
