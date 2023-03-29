0cb2bc0fce30fac0edc10f40793968737bda2607
derekhargest <derekhargest@gmail.com>
2023-03-29 12:07:45 -0400

    making adjustments for heroku

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import { Configuration, OpenAIApi } from "openai";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url'; // Import fileURLToPath from url module
+
+const __filename = fileURLToPath(import.meta.url); // Get the current file's path
+const __dirname = path.dirname(__filename); // Get the current directory's path
 
 const configuration = new Configuration({
-	organization: "org-Rqgad24MLeDu39gJKgZpH0XJ",
-	apiKey: "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",
+  organization: "org-Rqgad24MLeDu39gJKgZpH0XJ",
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -16,37 +21,34 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the public folder
+
 app.post("/", async (req, res) => {
-
 	const { messages } = req.body;
-
+  
 	console.log(messages);
-
+  
 	const completion = await openai.createChatCompletion({
-		model: "gpt-4",
-		messages: [
-			{ role: "system", content: "Youre a wise yogi in india. you have studied with all of the great minds on meditation and have read all of the books on meditation you can. I am going to ask you pointed questions about the nature of the warld and conciousness and i want you to respond to me like a wise yogi in the buddhist religion." },
-			...messages
-		]
+	  model: "gpt-4",
+	  messages: [
+		{
+		  role: "system",
+		  content:
+			"Youre a wise yogi in india. you have studied with all of the great minds on meditation and have read all of the books on meditation you can. I am going to ask you pointed questions about the nature of the warld and conciousness and i want you to respond to me like a wise yogi in the buddhist religion.",
+		},
+		...messages,
+	  ],
 	});
 	res.json({
-		completion: completion.data.choices[0].message
-	})
-	
-});
-
-app.listen(port, () => {
-	console.log(`Example app listening at http://0.0.0.0:${port}`)
-});
-
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            name: 'name of your app',
-            version: '0.1.0'
-        }
-    });
-
-});
+	  completion: completion.data.choices[0].message,
+	});
+  });
+  
+  app.get("/", (req, res) => {
+	// Serve the index.html file
+	res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+  
+  app.listen(port, () => {
+	console.log(`Example app listening at http://0.0.0.0:${port}`);
+  });
